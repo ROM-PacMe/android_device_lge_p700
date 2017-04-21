c5607a52748b1eebec3ebb6597ecc900a00b3b51
Rafael PacMe <rafy.pachecomendez@gmail.com>
2017-04-20 23:25:23 -0300

    rebrand GlazeOS

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,3 +19,6 @@ add_lunch_combo full_p700-userdebug
 add_lunch_combo full_p700-eng
 add_lunch_combo cm_p700-eng
 add_lunch_combo cm_p700-userdebug
+add_lunch_combo glaze_p700-eng
+add_lunch_combo glaze_p700-userdebug
+
